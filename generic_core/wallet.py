a4ce057e409642e98288b8b5868f50269a5400c3
FintechPH <fintech@peacehavencorp.com>
2026-09-21 22:06:47 +0530

    Update wallet.py

--- generic_core/wallet.py
+++ generic_core/wallet.py
@@ -1,1 +1,24 @@
 
+class Wallet:
+
+    def __init__(self):
+        self.gold_balance = 0.0
+
+    def add_gold(self, grams):
+
+        self.gold_balance += grams
+
+        return self.gold_balance
+
+    def remove_gold(self, grams):
+
+        if grams > self.gold_balance:
+            raise ValueError("Insufficient gold balance")
+
+        self.gold_balance -= grams
+
+        return self.gold_balance
+
+    def get_balance(self):
+
+        return self.gold_balance
